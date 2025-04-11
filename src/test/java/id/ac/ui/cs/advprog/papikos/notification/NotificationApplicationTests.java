e2d6c03f8fe7a704ea4ed3044b299ed50b5f9bc7
Adam <adamsembiring01@gmail.com>
2025-04-11 17:03:40 +0700

    init notification-wishlist service

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikos.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
