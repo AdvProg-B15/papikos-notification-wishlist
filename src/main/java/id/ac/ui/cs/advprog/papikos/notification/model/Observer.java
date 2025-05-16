395be2455f34a40684a2e680fb3f6cf257def813
Adam <adamsembiring01@gmail.com>
2025-05-16 17:49:46 +0700

    [RED] Change String to UUID

--- src/main/java/id/ac/ui/cs/advprog/papikos/notification/model/Observer.java
+++
@@ -1,6 +0,0 @@
-package id.ac.ui.cs.advprog.papikos.notification.model;
-
-public interface Observer {
-    void notify(Object subject);
-
-}
--- src/main/java/id/ac/ui/cs/advprog/papikos/notification/service/NotificationManager.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.papikos.notification.service;
-
-import id.ac.ui.cs.advprog.papikos.notification.model.Kos;
-
-public interface NotificationManager {
-    public void notifyUsers(Kos kos);
-}
