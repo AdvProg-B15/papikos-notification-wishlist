31c4779f7a95c1a728ec88ff0071ff51aef0c7d2
Adam <adamsembiring01@gmail.com>
2025-05-25 13:38:03 +0700

    add kos service client

---
+++ src/main/java/id/ac/ui/cs/advprog/papikos/notification/client/KosServiceClient.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.papikos.notification.client;
+
+import feign.Headers;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.UUID;
+
+@FeignClient(name = "kos-service", url = "${kos.service.url0:http://localhost:8081/api/v1}")
+public interface KosServiceClient {
+
+    @GetMapping("/{kosId}")
+    KosApiResponseWrapper<KosDetailsDto> getKosDetailsApiResponse(@PathVariable("kosId") UUID kosId);
+}
